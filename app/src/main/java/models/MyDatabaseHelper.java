0a5cd0317e1905f6fff6ad883a42112ae11af6c4
Essowe Agneketom <agnek2007@yahoo.fr>
2020-11-04 10:26:47 +0000

    5eme commit et correction des bugs apres changement de package

--- app/src/main/java/models/MyDatabaseHelper.java
+++ app/src/main/java/models/MyDatabaseHelper.java
@@ -6,11 +6,9 @@ import android.database.sqlite.SQLiteDatabase;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 public class MyDatabaseHelper extends SQLiteAssetHelper {
-    static final String DB_NAME="bibleunanv2.db";
-    static final int DB_VERSION=4;
+    static final String DB_NAME="plb.db";
+    static final int DB_VERSION=2;
     public MyDatabaseHelper(Context context) {
-
         super(context, DB_NAME, null, DB_VERSION);
-        setForcedUpgrade();
     }
 }
